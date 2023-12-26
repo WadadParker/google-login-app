7d4401762b3f4bc5fe0e94997a605f07a2c3cbdc
Wadad Parker <wadadparker5@gmail.com>
2023-12-26 08:00:21 +0530

    feat: env variables

--- App.js
+++ App.js
@@ -1,7 +1,3 @@
-// web 1097694407489-t04a451jdjr5flksdfcramdu93o5qhbj.apps.googleusercontent.com
-// ios 1097694407489-5fof1hr8f8v0pub897dsm38nmbf2m60q.apps.googleusercontent.com
-// android 1097694407489-958nbpoq4thdm92dj76l0of9lvmtgd9j.apps.googleusercontent.com
-
 import React, {useState,useEffect} from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
